5cdca2665c71f8a700d7b891be96bc7325eeab1f
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2016-05-02 11:37:19 +0200

    Added react hot loader.

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'public/app.js'
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
   },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'app.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: [
-          'react',
-          'es2015'
-        ]
-      }
+      loaders: ['react-hot', 'babel'],
+      exclude: path.join(__dirname, 'node_modules'),
+      include: path.join(__dirname, 'src')
     }, {
       test: /\.scss$/,
       loaders: ['style', 'css', 'sass']
